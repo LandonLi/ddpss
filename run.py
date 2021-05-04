8a911248290bd55373b7af9538b49077382d4bd3
Lava-Swimmer <lxx4work@gmail.com>
2021-05-05 03:24:37 +0800

    replace lxml by beautifulsoup4

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import logging
 from datetime import datetime
 
+from bs4 import BeautifulSoup
 from flask import Flask, jsonify, request
 from flask import current_app
-from lxml import etree
 
 from api import API
 
@@ -24,9 +24,9 @@ def read_root():
 @app.route("/subgroup")
 def subgroup():
     _, data = dmhyAPI.get_types_and_subgroups()
-    html = etree.HTML(data)
-    options = html.xpath('//select[@id="AdvSearchTeam"]/option')
-    subgroups = [{'Id': int(option.get('value')), 'Name': option.text} for option in options]
+    soup = BeautifulSoup(data, 'html.parser')
+    options = soup.find(id='AdvSearchTeam')
+    subgroups = [{'Id': int(option.get('value')), 'Name': option.text} for option in options.contents]
     subgroups.append({'Id': unknown_subgroup_id, 'Name': unknown_subgroup_name})
     return jsonify(Subgroups=subgroups)
 
@@ -34,9 +34,9 @@ def subgroup():
 @app.route("/type")
 def type():
     _, data = dmhyAPI.get_types_and_subgroups()
-    html = etree.HTML(data)
-    options = html.xpath('//select[@id="AdvSearchSort"]/option')
-    types = [{'Id': int(option.get('value')), 'Name': option.text} for option in options]
+    soup = BeautifulSoup(data, 'html.parser')
+    options = soup.find(id='AdvSearchSort')
+    types = [{'Id': int(option.get('value')), 'Name': option.text} for option in options.contents]
     return jsonify(Types=types)
 
 
@@ -47,35 +47,43 @@ def list():
     team_id = request.args.get('team_id')
     r = request.args.get('r')
     sort_id = int(sort_id) if sort_id else 0
-    team_id = int(team_id) if sort_id else 0
+    team_id = int(team_id) if team_id else 0
     _, data = dmhyAPI.search(keyword=keyword, sort_id=sort_id, team_id=team_id)
-    html = etree.HTML(data)
-    trs = html.xpath('//table[@id="topic_list"]/tbody/tr')
-    has_more = True if html.xpath('//div[@class="nav_title"]/a[contains(text(), "下一頁")]') else False
 
+    has_more = False
     resources = []
-    for tr in trs:
-        has_subgroup_info = (len(tr.xpath('./td[3]//a')) == 2)
-        try:
-            resource = {
-                'Title': ''.join(tr.xpath('./td[3]/a/text()')).strip(),
-                'TypeId': int(tr.xpath('./td[2]/a/@href')[0].split('/')[-1]),
-                'TypeName': tr.xpath('./td[2]/a/font')[0].text.strip(),
-                'SubgroupId': int(
-                    tr.xpath('./td[3]/span/a/@href')[0].split('/')[-1]) if has_subgroup_info else unknown_subgroup_id,
-                'SubgroupName': tr.xpath('./td[3]/span/a')[
-                    0].text.strip() if has_subgroup_info else unknown_subgroup_name,
-                'Magnet': tr.xpath('./td[4]/a/@href')[0],
-                'PageUrl': dmhyAPI.base_uri + tr.xpath('./td[3]/a/@href')[0],
-                'FileSize': tr.xpath('./td[5]')[0].text.strip(),
-                'PublishDate': datetime.strptime(tr.xpath('./td[1]/span')[0].text.strip(), '%Y/%m/%d %H:%M').strftime(
-                    '%Y-%m-%d %H:%M:%S')
-            }
-            resources.append(resource)
-        except ValueError:
-            pass
-        except Exception as e:
-            current_app.logger.error(e)
-            current_app.logger.error(etree.tostring(tr, pretty_print=True))
+    try:
+        soup = BeautifulSoup(data, 'html.parser')
+        trs = soup.find(id='topic_list').find_all('tr')[1:]
+        has_more = True if any(['下一頁' in getattr(div.find('a'), 'text', '') for div in
+                                soup.find_all('div', class_='nav_title')]) else False
 
-    return jsonify(HasMore=has_more, Resources=resources)
+        for tr in trs:
+            has_subgroup_info = (len(tr.find_all('td')[2].find_all('a')) == 2)
+            try:
+                resource = {
+                    'Title': tr.find_all('td')[2].find_all('a')[-1].text.strip(),
+                    'TypeId': int(tr.find_all('td')[1].a.get('href').split('/')[-1]),
+                    'TypeName': tr.find_all('td')[1].a.text.strip(),
+                    'SubgroupId': int(tr.find_all('td')[2].find_all('a')[0].get('href').split('/')[
+                                          -1]) if has_subgroup_info else unknown_subgroup_id,
+                    'SubgroupName': tr.find_all('td')[2].find_all('a')[
+                        0].text.strip() if has_subgroup_info else unknown_subgroup_name,
+                    'Magnet': tr.find_all('td')[3].a.get('href'),
+                    'PageUrl': dmhyAPI.base_uri + tr.find_all('td')[2].find_all('a')[-1].get('href'),
+                    'FileSize': tr.find_all('td')[4].text.strip(),
+                    'PublishDate': datetime.strptime(
+                        tr.find_all('td')[0].span.text.strip(), '%Y/%m/%d %H:%M'
+                    ).strftime('%Y-%m-%d %H:%M:%S')
+                }
+                resources.append(resource)
+            except ValueError:
+                pass
+            except Exception as e:
+                current_app.logger.error(e)
+                current_app.logger.error(tr.pretty())
+    except Exception as e:
+        current_app.logger.error(e)
+        current_app.logger.error("无法解析结果")
+    finally:
+        return jsonify(HasMore=has_more, Resources=resources)
